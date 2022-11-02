36d1f9dd578d7680d0adf8fe9d50c12cc9709e45
Shakti Singh Rathore <dxxshaktirathore@gmail.com>
2022-11-02 18:36:15 +0530

    check prime and pailandrome

---
+++ plaindrome.js
@@ -0,0 +1,12 @@
+let str="naman"
+ let bag="";
+ for(let i=str.length-1;i>=0;i--){
+	bag+=str[i];
+ }
+  if(str==bag){
+	console.log("pailendrome");
+  } else{
+        console.log("not pailendrome");
+  }
+
+	
---
+++ prime.js
@@ -0,0 +1,12 @@
+let num=8;
+let count=0;
+for(let i=i;i<=num;i++){
+	if(num%i){
+	count++;
+	}
+}
+if(count==2){
+ console.log("prime");
+} else{
+	console.log("not prime");
+}
